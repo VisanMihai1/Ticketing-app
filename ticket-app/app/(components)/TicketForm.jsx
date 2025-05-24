7bc69d8bb6d4dbc82baa4078852b0f22298e4182
Visan Mihai <visanmihaigabri@gmail.com>
2025-05-24 16:10:56 +0300

    feat: enhance TicketForm with submission handling and additional fields for description, category, priority, progress, and status

--- ticket-app/app/(components)/TicketForm.jsx
+++ ticket-app/app/(components)/TicketForm.jsx
@@ -17,6 +17,10 @@ const TicketForm = ({ ticket }) => {
 
   const [formData, setFormData] = useState(startingTicketData);
 
+  const handleSubmit = () => {
+    console.log("submitted");
+  };
+
   const handleChange = (e) => {
     const value = e.target.value;
     const name = e.target.name;
@@ -29,7 +33,11 @@ const TicketForm = ({ ticket }) => {
 
   return (
     <div className="flex justify-center">
-      <form className="flex flex-col gap-3 w-1/2">
+      <form
+        className="flex flex-col gap-3 w-1/2"
+        method="post"
+        onSubmit={handleSubmit}
+      >
         <h3>Create Your Ticket</h3>
         <label>Title</label>
         <input
@@ -40,6 +48,102 @@ const TicketForm = ({ ticket }) => {
           required={true}
           value={formData.title}
         />
+
+        <label>Description</label>
+        <textarea
+          id="Description"
+          name="Description"
+          onChange={handleChange}
+          required={true}
+          value={formData.Description}
+          rows="5"
+        />
+
+        <label>Category</label>
+        <select
+          name="Category"
+          value={formData.Category}
+          onChange={handleChange}
+        >
+          <option value="Hardware Problem">Hardware Problem</option>
+          <option value="Software Problem">Software Problem</option>
+          <option value="Project">Project</option>
+          <option value="Miscellaneous">Miscellaneous</option>
+        </select>
+
+        <label>Priority</label>
+        <div>
+          <input
+            id="priority-1"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={1}
+            checked={formData.priority == 1}
+          />
+          <label>1</label>
+        </div>
+        <div>
+          <input
+            id="priority-2"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label>2</label>
+        </div>
+        <div>
+          <input
+            id="priority-3"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label>3</label>
+        </div>
+        <div>
+          <input
+            id="priority-4"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={4}
+            checked={formData.priority == 4}
+          />
+          <label>4</label>
+        </div>
+        <div>
+          <input
+            id="priority-5"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={5}
+            checked={formData.priority == 5}
+          />
+          <label>5</label>
+        </div>
+        <label>Progress</label>
+        <input
+          type="range"
+          id="progress"
+          name="progress"
+          value={formData.progress}
+          min="o"
+          max="100"
+          onChange={handleChange}
+        />
+        <label>Status</label>
+        <select name="status" value={formData.status} onChange={handleChange}>
+          <option value="not started">Not started</option>
+          <option value="started">Started</option>
+          <option value="done">Done</option>
+        </select>
+        <input type="submit" className="btn" value="Create ticket" />
       </form>
     </div>
   );
